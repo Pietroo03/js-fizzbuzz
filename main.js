2797af6dad468684330cd1c9d47d386135f38626
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-01 14:45:13 +0200

    add log "Buzz" for "i" multiple of 5

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ console.log('ciao');
 
     if (i % 3 === 0 ) {
         console.log('Fizz');
+    } else if (i % 5 === 0){
+        console.log('Buzz')
     } else {
-        console.log(i)
+        console.log(i);
+        
     }
 }
