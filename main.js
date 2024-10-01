f1f41e8b0d49086cc622767d72ae1cce8391cb3a
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-01 14:37:07 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('ciao');
+
+
+/*Scrivi un programma che stampi i numeri da 1 a 100:
+ - per i multipli di 3 stampi “Fizz” al posto del numero 
+ - per i multipli di 5 stampi Buzz 
+ - Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
+
